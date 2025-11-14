e003451c9b7a7b8bfcd4ac9b7a2fa57b253efe02
camilo <camilo171512@gmail.com>
2025-11-13 19:52:44 -0500

    agrege un try catch

--- src/app/App.java
+++ src/app/App.java
@@ -5,23 +5,28 @@ import domain.Producto;
 import service.Limoncito;
 
 public class app {
+
     public static void main(String[] args) {
-        
-        var service = new Limoncito();
-        var cliente = new Cliente("Ana", "3001234567");
-        var pedido = service.crearPedido(cliente);
-        
-        service.agregarItem(pedido, Producto.CAMISA, 2);
-        service.agregarItem(pedido, Producto.PANTALON, 2);
-        
-        // Servicio Expres
-        pedido.setServicioExpres(false);
-        
-        System.out.println(service.resumen(pedido));
-        
-        // CONFIRMAR
-        service.confirmar(pedido);
-        System.out.println("Pedido confirmado: " + pedido.getEstado());
+        try {
+            var service = new Limoncito();
+            var cliente = new Cliente("Ana", "3001234567");
+            var pedido = service.crearPedido(cliente);
+
+            service.agregarItem(pedido, Producto.CAMISA, 3);
+            service.agregarItem(pedido, Producto.PANTALON, 2);
+
+            // Servicio Expres
+            pedido.setServicioExpres(false);
+
+            System.out.println(service.resumen(pedido));
+
+            // CONFIRMAR
+            service.confirmar(pedido);
+            System.out.println("Pedido confirmado: " + pedido.getEstado());
+
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            System.err.println("Error al procesar el pedido: " + e.getMessage());
+        }
 
     }
 }
